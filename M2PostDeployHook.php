76ababb3c812c1581d902e934949af393858db1e
Omri de Hek <omridehek@comaxx.nl>
2017-02-21 08:46:42 +0100

    Small bugfixes for magento post deploy hook

--- M2PostDeployHook.php
+++ M2PostDeployHook.php
@@ -15,12 +15,12 @@ class M2PostDeployHook implements Hooks_DeployerInterface
     public function postDeployer(Deployer &$deployer)
     {
         NedStars_Log::message('Running Post Deploy commands:');
-        $bashCMD[] = "composer.phar install";
-        $bashCMD[] = "./bin/magento setup:static-content:deploy nl_NL en_US";
-        $bashCMD[] = "./bin/magento setup:upgrade";
-        $bashCMD[] = "./bin/magento setup:di:compile";
-        $bashCMD[] = "./bin/magento maintenance:disable"; //always disable maintainance mode
-        $bashCMD[] = "./bin/magento cache:flush";
+        $bashCMD[] = "composer.phar install 2>1 > /dev/null";
+        $bashCMD[] = "bin/magento setup:static-content:deploy nl_NL en_US 2>1 > /dev/null";
+        $bashCMD[] = "bin/magento setup:upgrade 2>1 > /dev/null";
+        $bashCMD[] = "bin/magento setup:di:compile 2>1 > /dev/null";
+        $bashCMD[] = "bin/magento maintenance:disable 2>1 > /dev/null"; //always disable maintainance mode
+        $bashCMD[] = "bin/magento cache:flush 2>1 > /dev/null";
 
         $this->runBashCommand($deployer, $bashCMD);
     }
@@ -35,10 +35,9 @@ class M2PostDeployHook implements Hooks_DeployerInterface
         $path = (string)$deployer->getConfig()->paths->web_live_path;
         foreach ($bashCMD as $_bashCMD) {
             $output = array();
-            $return = 0; //composer.phar install
+            $return = 0;
             $command = "cd {$path} && $(which php) {$_bashCMD}";
             exec($command, $output, $return);
-            NedStars_Log::message('Output: ' . implode(PHP_EOL, $output));
         }
         return $this;
     }
